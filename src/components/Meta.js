c83e0da235fe361ffaf89004b5de29abb73b5563
l6174 <l.lawliet6174@gmail.com>
2023-04-17 08:53:50 +0530

    Changed Favicon and Title

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -7,7 +7,7 @@ const Meta = () => {
 	const { settings } = useSettings()
 
 	useEffect(() => {
-		setTitle(settings.username + " Start Page")
+		setTitle("Start Page")
 	}, [settings.username])
 
 	return (
